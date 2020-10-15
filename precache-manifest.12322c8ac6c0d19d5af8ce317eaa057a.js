5cf5c96a25a54a0bd251a8c2a4b7d7381ba0c8a7
May Thun <may.th@Mays-MacBook.local>
2020-10-16 00:39:35 +0300

    Updates

---
+++ precache-manifest.12322c8ac6c0d19d5af8ce317eaa057a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e047a390841653e226b3bac870102b6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab318ecaba4c23e82ba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1dcef912b756337d95f9",
+    "url": "/static/js/2.89c9c2c1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.89c9c2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab318ecaba4c23e82ba",
+    "url": "/static/js/main.a96492e4.chunk.js"
+  },
+  {
+    "revision": "7cf029912127f35872f1",
+    "url": "/static/js/runtime-main.93e4e16f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
