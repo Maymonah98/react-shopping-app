c12ced63d61a65def6b0d8057ac9fde9a00b5987
May Thun <may.th@Mays-MacBook.local>
2020-11-04 20:38:34 +0300

    ..

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,54 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Favorite from '@material-ui/icons/Favorite';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import Navbar from '../Navbar'
 
 
 function Header() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}> 
-        <Toolbar>
-        <Button >Login</Button>
-        <Button >sign up</Button>
-          <Typography  
-          component="h2"
-          variant="h5"
-          align="center"
-           className={classes.title}>
-            HandMade 
-          </Typography>
-          {/* <IconButton edge="start" className={classes.menuButton} aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
-          <IconButton edge="start"  className={classes.menuButton} color="inherit"  aria-label="fovorite">
-            <Favorite/>
-          </IconButton>
-          <IconButton edge="start"  className={classes.menuButton} color="inherit"  aria-label="shoppingBasket">
-            <ShoppingBasket/>
-          </IconButton>
-        </Toolbar>
-    </div>
-  );
+    return (
+      <div >
+        <Navbar/>
+      
+      </div>
+    );
   }
 
   export default Header;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Favorite from '@material-ui/icons/Favorite';
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 
 function Navbar() {
-    return (
-      <div >
-        hello
-      
-      </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}> 
+        <Toolbar>
+        <Button >Login</Button>
+        <Button >sign up</Button>
+          <Typography  
+          component="h2"
+          variant="h5"
+          align="center"
+           className={classes.title}>
+            HandMade 
+          </Typography>
+          {/* <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+            <MenuIcon />
+          </IconButton> */}
+          <IconButton edge="start"  className={classes.menuButton} color="inherit"  aria-label="fovorite">
+            <Favorite/>
+          </IconButton>
+          <IconButton edge="start"  className={classes.menuButton} color="inherit"  aria-label="shoppingBasket">
+            <ShoppingBasket/>
+          </IconButton>
+        </Toolbar>
+    </div>
+  );
   }
 
   export default Navbar;
