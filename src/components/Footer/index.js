bd21ba4f0de75c68066d268bc330d3d887b96637
May Thun <may.th@Mays-MacBook.local>
2020-11-04 19:39:57 +0300

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
-
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    // marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
+  },
+}));
 
 function Footer() {
+  const classes = useStyles();
+
     return (
-      <div >
-        hello
-      
-      </div>
+      <footer className={classes.footer}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+        Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        Something here to give the footer a purpose!
+        </Typography>
+      </Container>
+    </footer>
     );
   }
 
