8ec4281b2ce9288d77dbcf55bd8c18aa32b64817
Maymonah Althunayan <maythun15@gmail.com>
2021-05-23 07:23:49 +0300

    add menubar UI

--- src/components/Header/MenuBar/index.js
+++ src/components/Header/MenuBar/index.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
+
+
+
+const sections = [
+    { title: 'Technology', url: '#' },
+    { title: 'Design', url: '#' },
+    { title: 'Culture', url: '#' },
+    { title: 'Business', url: '#' },
+    { title: 'Politics', url: '#' },
+    { title: 'Opinion', url: '#' },
+    { title: 'Science', url: '#' },
+    { title: 'Health', url: '#' },
+    { title: 'Style', url: '#' },
+    { title: 'Travel', url: '#' },
+  ];
+
+const useStyles = makeStyles((theme) => ({
+    toolbar: {
+    //   borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbarTitle: {
+    //   flex: 1,
+    },
+    toolbarSecondary: {
+      justifyContent: 'space-between',
+      overflowX: 'auto',
+      backgroundColor:"black"
+    },
+    toolbarLink: {
+      padding: theme.spacing(1),
+      color:"white",
+      flexShrink: 0,
+    },
+  }));
 
 function MenuBar() {
+    const classes = useStyles();
+
     return (
         <div >
-         menuBar
+            <Toolbar className={classes.toolbarSecondary}>
+            {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            className={classes.toolbarLink}
+          >
+            {section.title}
+          </Link>
+            ))}
+            </Toolbar>
+         
         </div>
       );
 }
