9a681fda5133d5ebd5a346cf896189fc54f5752f
Maymonah Althunayan <maythun15@gmail.com>
2021-05-27 03:05:36 +0300

    add routers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import HomePage from './components/HomePage';
 import './App.css';
-
+import {Route , Switch} from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
 function App() {
   return (
+    <Switch>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+
+    <Route exact path="/">
+
     <div className="App">
       <HomePage/>
-    
     </div>
+    </Route>
+    </Switch>
   );
 }
 
